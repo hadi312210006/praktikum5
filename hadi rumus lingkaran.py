1f6b647d491487cc3a2318e9d628b86e85f16622
hadi312210006 <117914408+hadi312210006@users.noreply.github.com>
2022-12-23 20:39:48 +0700

    Add files via upload

---
+++ hadi rumus lingkaran.py
@@ -0,0 +1,7 @@
+#rumus lingkaran
+phi=3.14
+r=float(input('masukan jari2 lingkaran'))
+luas=phi*r*r
+print('luas lingkaran adalah:'+str(luas))
+k=2*phi*r
+print('keliling lingkaran adalah{}'.format(k))
